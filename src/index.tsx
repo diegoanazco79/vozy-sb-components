06c975fe320614b6fd157284775e69d31b40ec8b
Juan González <jgonzalez@vozy.co>
2023-02-10 14:07:34 -0500

    :sparkles: NEW FEATURE :: Adding Button and InteractiveBox in main index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import '@coreui/coreui/dist/css/coreui.min.css';
 
-export { VzBadge } from './components/Badge'
-export { VzCheckbox } from './components/Checkbox'
-export { VzLabel } from './components/Label'
-export { VzLoadingData } from './components/LoadingData'
-export { VzPagination } from './components/Pagination'
-export { VzRadioButton } from './components/RadioButton'
-export { VzSteps } from './components/Steps'
-export { VzSwitch } from './components/Switch'
+export { VzBadge } from './components/Badge';
+export { VzCheckbox } from './components/Checkbox';
+export { VzLabel } from './components/Label';
+export { VzLoadingData } from './components/LoadingData';
+export { VzPagination } from './components/Pagination';
+export { VzRadioButton } from './components/RadioButton';
+export { VzSteps } from './components/Steps';
+export { VzSwitch } from './components/Switch';
+export { VzButton } from './components/Button';
+export { VzInteractiveBox } from './components/InteractiveBox';
